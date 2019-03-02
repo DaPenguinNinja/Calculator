e77bb7036e8352c64f52e1ee8e866d361a20ef64
Peter O <okwuosah.2@gmail.com>
2019-03-01 20:19:25 -0500

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,10 @@
+#Tuples are immutable. Can't change values
+coordinates = (4,5)
+coor = [(1,2), (4,6), (8,4)]
+print(coordinates)
+print(coordinates[1])
+print(coordinates[0])
+
+print(coor)
+print(coor[2])
+print(coor[2][1])
